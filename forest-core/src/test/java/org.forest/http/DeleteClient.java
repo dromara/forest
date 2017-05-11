6ef28c4e30005b9f3dea1c0c09a06ff048b94de3
jun.gong <jun.gong@thebeastshop.com>
2017-05-11 17:45:44 +0800

    添加mockserver的测试: Delete

---
+++ forest-core/src/test/java/org.forest/http/DeleteClient.java
@@ -0,0 +1,36 @@
+package org.forest.http;
+
+import org.forest.annotation.DataParam;
+import org.forest.annotation.Request;
+
+/**
+ * @author gongjun[jun.gong@thebeastshop.com]
+ * @since 2017-05-11 17:22
+ */
+public interface DeleteClient {
+
+    @Request(
+            url = "http://localhost:5000/xx/user?username=foo",
+            type = "delete",
+            headers = {"Accept:text/plan"}
+    )
+    String simpleDelete();
+
+
+    @Request(
+            url = "http://localhost:5000/xx/user",
+            type = "delete",
+            headers = {"Accept:text/plan"},
+            data = "username=${0}"
+    )
+    String textParamDelete(String username);
+
+
+    @Request(
+            url = "http://localhost:5000/xx/user",
+            type = "delete",
+            headers = {"Accept:text/plan"}
+    )
+    String annParamDelete(@DataParam("username") String usernam);
+
+}
--- forest-core/src/test/java/org.forest/http/TestDeleteClient.java
+++ forest-core/src/test/java/org.forest/http/TestDeleteClient.java
@@ -12,45 +12,44 @@ import org.mockserver.model.Header;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Map;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
 /**
  * @author gongjun[jun.gong@thebeastshop.com]
- * @since 2017-05-11 15:02
+ * @since 2017-05-11 17:24
  */
-public class TestGetClient {
+public class TestDeleteClient {
 
-    private final static Logger log = LoggerFactory.getLogger(TestGetClient.class);
+    private final static Logger log = LoggerFactory.getLogger(TestDeleteClient.class);
 
     @Rule
     public MockServerRule server = new MockServerRule(this, 5000);
 
     private static ForestConfiguration configuration;
 
-    private static GetClient getClient;
+    private static DeleteClient deleteClient;
 
     private final static String expected = "{\"status\": \"ok\"}";
+
 
     @BeforeClass
     public static void prepareClient() {
         configuration = ForestConfiguration.configuration();
-        getClient = configuration.createInstance(GetClient.class);
+        deleteClient = configuration.createInstance(DeleteClient.class);
     }
-
 
     @Before
     public void prepareMockServer() {
         MockServerClient mockClient = new MockServerClient("localhost", 5000);
         mockClient.when(
                 request()
-                        .withPath("/hello/foo")
-                        .withMethod("GET")
+                        .withPath("/xx/user")
+                        .withMethod("DELETE")
                         .withHeader(new Header(HttpHeaders.ACCEPT, "text/plan"))
+                        .withQueryStringParameter("username", "foo")
         ).respond(
                 response()
                         .withStatusCode(200)
@@ -59,19 +58,27 @@ public class TestGetClient {
     }
 
     @Test
-    public void testGet() {
-        String result = getClient.simpleGet();
+    public void testSimpleDelete() {
+        String result = deleteClient.simpleDelete();
         log.info("response: " + result);
         assertNotNull(result);
         assertEquals(expected, result);
     }
 
+    @Test
+    public void testTextParamDelete() {
+        String result = deleteClient.textParamDelete("foo");
+        log.info("response: " + result);
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
 
     @Test
-    public void testJsonMapGet() {
-        Map map = getClient.jsonMapGet();
-        assertNotNull(map);
-        assertEquals("ok", map.get("status"));
+    public void testAnnParamDelete() {
+        String result = deleteClient.annParamDelete("foo");
+        log.info("response: " + result);
+        assertNotNull(result);
+        assertEquals(expected, result);
     }
 
 }
