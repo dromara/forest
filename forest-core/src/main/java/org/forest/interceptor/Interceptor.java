34e0862788718e2d2899739de8c3ac7975e2d4f9
jun.gong <jun.gong@thebeastshop.com>
2017-05-12 18:55:48 +0800

    添加拦截器

--- forest-core/src/main/java/org/forest/interceptor/Interceptor.java
+++ forest-core/src/main/java/org/forest/interceptor/Interceptor.java
@@ -9,14 +9,14 @@ import org.forest.exceptions.ForestRuntimeException;
  * @author gongjun[dt_flys@hotmail.com]
  * @since 2016-06-26
  */
-public interface ForestInterceptor {
+public interface Interceptor {
 
-    boolean beforeExecute(ForestMethod method, ForestRequest request);
+    boolean beforeExecute(ForestRequest request);
 
     void onSuccess(Object data, ForestRequest request, ForestResponse response);
 
-    void onError(ForestRuntimeException ex, ForestRequest request);
+    void onError(ForestRuntimeException ex, ForestResponse response);
 
-    void onCompleted(ForestMethod method, ForestRequest request, ForestResponse response);
+    void afterExecute(ForestRequest request, ForestResponse response);
 
 }
---
+++ forest-core/src/main/java/org/forest/interceptor/InterceptorChain.java
@@ -0,0 +1,66 @@
+package org.forest.interceptor;
+
+import org.forest.exceptions.ForestRuntimeException;
+import org.forest.http.ForestRequest;
+import org.forest.http.ForestResponse;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * @author gongjun[jun.gong@thebeastshop.com]
+ * @since 2017-05-12 18:30
+ */
+public class InterceptorChain implements Interceptor {
+
+    private LinkedList<Interceptor> interceptors = new LinkedList<>();
+
+    public InterceptorChain addInteceptor(Interceptor interceptor) {
+        interceptors.add(interceptor);
+        return this;
+    }
+
+    public int getInteceptorSize() {
+        return interceptors.size();
+    }
+
+    @Override
+    public boolean beforeExecute(ForestRequest request) {
+        Iterator<Interceptor> iter = interceptors.iterator();
+        for ( ; iter.hasNext(); ) {
+            Interceptor item = iter.next();
+            boolean result = item.beforeExecute(request);
+            if (!result) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onSuccess(Object data, ForestRequest request, ForestResponse response) {
+        Iterator<Interceptor> iter = interceptors.iterator();
+        for (; iter.hasNext(); ) {
+            Interceptor item = iter.next();
+            item.onSuccess(data, request, response);
+        }
+    }
+
+    @Override
+    public void onError(ForestRuntimeException ex, ForestResponse response) {
+        Iterator<Interceptor> iter = interceptors.iterator();
+        for ( ; iter.hasNext(); ) {
+            Interceptor item = iter.next();
+            item.onError(ex, response);
+        }
+    }
+
+    @Override
+    public void afterExecute(ForestRequest request, ForestResponse response) {
+        Iterator<Interceptor> iter = interceptors.iterator();
+        for ( ; iter.hasNext(); ) {
+            Interceptor item = iter.next();
+            item.afterExecute(request, response);
+        }
+    }
+}
