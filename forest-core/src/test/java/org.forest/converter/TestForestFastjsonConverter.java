5b9f5c19380a337f47ab86c87462e05ee4e505f6
jun.gong <jun.gong@thebeastshop.com>
2017-05-18 13:11:06 +0800

    测试fastjson converter

--- forest-core/src/test/java/org.forest/converter/TestForestFastjsonConverter.java
+++ forest-core/src/test/java/org.forest/converter/TestForestFastjsonConverter.java
@@ -1,5 +1,6 @@
 package org.forest.converter;
 
+import com.alibaba.fastjson.TypeReference;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import junit.framework.Assert;
 import org.forest.converter.json.ForestFastjsonConverter;
@@ -7,6 +8,10 @@ import org.forest.exceptions.ForestRuntimeException;
 import org.junit.Test;
 
 import java.util.Map;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
 
 /**
  * @author gongjun[dt_flys@hotmail.com]
@@ -19,21 +24,21 @@ public class TestForestFastjsonConverter {
         ForestFastjsonConverter forestFastjsonConverter = new ForestFastjsonConverter();
         String defaultSerializerFeatureName = forestFastjsonConverter.getSerializerFeatureName();
         SerializerFeature defaultSerializerFeature = forestFastjsonConverter.getSerializerFeature();
-        Assert.assertEquals(SerializerFeature.DisableCircularReferenceDetect.name(),
+        assertEquals(SerializerFeature.DisableCircularReferenceDetect.name(),
                 defaultSerializerFeatureName);
-        Assert.assertEquals(defaultSerializerFeature.name(),
+        assertEquals(defaultSerializerFeature.name(),
                 defaultSerializerFeatureName);
 
         forestFastjsonConverter.setSerializerFeatureName(SerializerFeature.WriteClassName.name());
-        Assert.assertEquals(SerializerFeature.WriteClassName.name(),
+        assertEquals(SerializerFeature.WriteClassName.name(),
                 forestFastjsonConverter.getSerializerFeatureName());
-        Assert.assertEquals(SerializerFeature.WriteClassName,
+        assertEquals(SerializerFeature.WriteClassName,
                 forestFastjsonConverter.getSerializerFeature());
 
         forestFastjsonConverter.setSerializerFeature(SerializerFeature.BeanToArray);
-        Assert.assertEquals(SerializerFeature.BeanToArray.name(),
+        assertEquals(SerializerFeature.BeanToArray.name(),
                 forestFastjsonConverter.getSerializerFeatureName());
-        Assert.assertEquals(SerializerFeature.BeanToArray,
+        assertEquals(SerializerFeature.BeanToArray,
                 forestFastjsonConverter.getSerializerFeature());
     }
 
@@ -41,20 +46,61 @@ public class TestForestFastjsonConverter {
     public void testConvertToJson() {
         ForestFastjsonConverter forestFastjsonConverter = new ForestFastjsonConverter();
         String text = forestFastjsonConverter.convertToJson(new Integer[] {100, 10});
-        Assert.assertEquals("[100,10]", text);
+        assertEquals("[100,10]", text);
     }
 
     @Test
-    public void testConverterError() {
-        String badJsonText = "{\"a\"=1";
+    public void testConvertToJava() {
+        String jsonText = "{\"a\":1, \"b\":2}";
+        ForestFastjsonConverter forestFastjsonConverter = new ForestFastjsonConverter();
+        Map result = forestFastjsonConverter.convertToJavaObject(jsonText, Map.class);
+        assertNotNull(result);
+        assertEquals(1, result.get("a"));
+        assertEquals(2, result.get("b"));
+
+        result = forestFastjsonConverter.convertToJavaObject(jsonText, new TypeReference<Map>() {}.getType());
+        assertNotNull(result);
+        assertEquals(1, result.get("a"));
+        assertEquals(2, result.get("b"));
+
+        result = forestFastjsonConverter.convertToJavaObject(jsonText, new TypeReference<Map>() {});
+        assertNotNull(result);
+        assertEquals(1, result.get("a"));
+        assertEquals(2, result.get("b"));
+
+    }
+
+    @Test
+    public void testConvertToJavaError() {
+        String badJsonText = "{\"a\":1";
         ForestFastjsonConverter forestFastjsonConverter = new ForestFastjsonConverter();
         boolean error = false;
         try {
             forestFastjsonConverter.convertToJavaObject(badJsonText, Map.class);
         } catch (ForestRuntimeException e) {
             error = true;
+            assertNotNull(e.getCause());
         }
-        Assert.assertTrue(error);
+        assertTrue(error);
+
+        error = true;
+        try {
+            forestFastjsonConverter.convertToJavaObject(badJsonText, new TypeReference<Map>() {}.getType());
+        } catch (ForestRuntimeException e) {
+            error = true;
+            assertNotNull(e.getCause());
+        }
+        assertTrue(error);
+
+        error = true;
+        try {
+            forestFastjsonConverter.convertToJavaObject(badJsonText, new TypeReference<Map>() {});
+        } catch (ForestRuntimeException e) {
+            error = true;
+            assertNotNull(e.getCause());
+        }
+        assertTrue(error);
+
     }
 
 }
