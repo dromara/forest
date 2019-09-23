75ece0f799fdd61b317bc46eb6a48bcfeb1d2d23
jun.gong <jun.gong@thebeastshop.com>
2019-09-23 19:35:20 +0800

    new version 1.1.2

--- forest-core/src/main/java/com/dtflys/forest/backend/okhttp3/executor/AbstractOkHttp3Executor.java
+++ forest-core/src/main/java/com/dtflys/forest/backend/okhttp3/executor/AbstractOkHttp3Executor.java
@@ -6,6 +6,7 @@ import com.dtflys.forest.backend.url.URLBuilder;
 import com.dtflys.forest.http.ForestRequest;
 import com.dtflys.forest.http.ForestResponse;
 import com.dtflys.forest.utils.RequestNameValue;
+import com.dtflys.forest.utils.StringUtils;
 import okhttp3.*;
 import com.dtflys.forest.backend.okhttp3.conn.OkHttp3ConnectionManager;
 import com.dtflys.forest.backend.okhttp3.response.OkHttp3ForestResponseFactory;
@@ -15,9 +16,15 @@ import com.dtflys.forest.converter.json.ForestJsonConverter;
 import com.dtflys.forest.exceptions.ForestNetworkException;
 import com.dtflys.forest.handler.ResponseHandler;
 import com.dtflys.forest.mapping.MappingTemplate;
+import okio.BufferedSink;
+import okio.Okio;
+import okio.Sink;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
+
 
 /**
  * @author gongjun[jun.gong@thebeastshop.com]
@@ -25,11 +32,101 @@ import java.util.List;
  */
 public abstract class AbstractOkHttp3Executor implements HttpExecutor {
 
+    private final static Log log = LogFactory.getLog(AbstractOkHttp3Executor.class);
+
     protected final ForestRequest request;
 
     private final OkHttp3ConnectionManager connectionManager;
 
     private final OkHttp3ResponseHandler okHttp3ResponseHandler;
+
+    protected String getLogContentForRequestLine(int retryCount, Request okRequest) {
+        HttpUrl url = okRequest.url();
+        String scheme = url.scheme().toUpperCase();
+        String uri = url.uri().toString();
+        String method = okRequest.method();
+        String httpline = method + " " + uri + " " + scheme;
+        if (retryCount == 0) {
+            return httpline;
+        }
+        else {
+            return "[Retry: " + retryCount + "] " + httpline;
+        }
+    }
+
+    protected String getLogContentForHeaders(Request okRequest) {
+        StringBuffer buffer = new StringBuffer();
+        Headers headers = okRequest.headers();
+        for (int i = 0; i < headers.size(); i++) {
+            String name = headers.name(i);
+            String value = headers.value(i);
+            buffer.append("\t\t" + name + ": " + value);
+            if (i < headers.size() - 1) {
+                buffer.append("\n");
+            }
+        }
+        return buffer.toString();
+    }
+
+
+    protected String getLogContentForBody(Request okRequest) {
+        RequestBody requestBody = okRequest.body();
+        if (requestBody == null) {
+            return null;
+        }
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Sink sink = Okio.sink(out);
+        BufferedSink bufferedSink = Okio.buffer(sink);
+        try {
+            requestBody.writeTo(bufferedSink);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferedSink.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        InputStream inputStream = new ByteArrayInputStream(out.toByteArray());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuffer buffer = new StringBuffer();
+        String line;
+        String body;
+        try {
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + " ");
+            }
+            body = buffer.toString();
+            return body;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+
+
+    protected static void logContent(String content) {
+        log.info("[Forest] " + content);
+    }
+
+    public void logRequest(int retryCount,  Request okRequest) {
+        if (!request.isLogEnable()) return;
+        okRequest.url().uri().toString();
+        String requestLine = getLogContentForRequestLine(retryCount, okRequest);
+        String headers = getLogContentForHeaders(okRequest);
+        String body = getLogContentForBody(okRequest);
+        String content = "Request: \n\t" + requestLine;
+        if (StringUtils.isNotEmpty(headers)) {
+            content += "\n\tHeaders: \n" + headers;
+        }
+        if (StringUtils.isNotEmpty(body)) {
+            content += "\n\tBody: " + body;
+        }
+        logContent(content);
+    }
+
 
 
     protected AbstractOkHttp3Executor(ForestRequest request, OkHttp3ConnectionManager connectionManager, OkHttp3ResponseHandler okHttp3ResponseHandler) {
@@ -78,7 +175,7 @@ public abstract class AbstractOkHttp3Executor implements HttpExecutor {
         final Request okRequest = builder.build();
         Call call = okHttpClient.newCall(okRequest);
         final OkHttp3ForestResponseFactory factory = new OkHttp3ForestResponseFactory();
-
+        logRequest(0, okRequest);
         if (request.isAsync()) {
             final OkHttp3ResponseFuture future = new OkHttp3ResponseFuture();
             call.enqueue(new Callback() {
