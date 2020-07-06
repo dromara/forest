85f6f9ea6714aac18159d0507f290ae50472417f
jun.gong <jun.gong@thebeastshop.com>
2020-07-06 19:21:42 +0800

    refactor: 添加@Documented

--- forest-core/src/main/java/com/dtflys/forest/annotation/Request.java
+++ forest-core/src/main/java/com/dtflys/forest/annotation/Request.java
@@ -24,15 +24,13 @@
 
 package com.dtflys.forest.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * @author gongjun[dt_flys@hotmail.com]
  * @since 2016-05-12
  */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Request {
