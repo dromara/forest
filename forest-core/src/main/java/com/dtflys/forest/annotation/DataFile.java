5fa9a53846a69be816685180dd8792d8afcb13ce
mySIngleLive <dt_flys@hotmail.com>
2020-07-26 16:40:28 +0800

    feat: 添加@DataFile注解

---
+++ forest-core/src/main/java/com/dtflys/forest/annotation/DataFile.java
@@ -0,0 +1,13 @@
+package com.dtflys.forest.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author gongjun
+ * @since 2020-07-26
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface DataFile {
+}
