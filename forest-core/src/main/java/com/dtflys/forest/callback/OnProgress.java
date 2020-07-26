66316494addfddeb2902fcee7be0e660adcfbe98
mySIngleLive <dt_flys@hotmail.com>
2020-07-26 23:58:07 +0800

    feat: 添加OnProgress回调函数

---
+++ forest-core/src/main/java/com/dtflys/forest/callback/OnProgress.java
@@ -0,0 +1,12 @@
+package com.dtflys.forest.callback;
+
+import com.dtflys.forest.utils.ForestProgress;
+
+/**
+ * @author gongjun
+ * @since 2020-07-26
+ */
+public interface OnProgress {
+
+    void onProgress(ForestProgress progress);
+}
