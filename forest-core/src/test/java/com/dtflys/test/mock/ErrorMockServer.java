2ed84260448427cc1028157d0344932ebb370702
jun.gong <jun.gong@thebeastshop.com>
2020-07-29 15:48:00 +0800

    test: 多层泛型嵌套返回类型

---
+++ forest-core/src/test/java/com/dtflys/test/mock/ErrorMockServer.java
@@ -0,0 +1,42 @@
+package com.dtflys.test.mock;
+
+import org.apache.http.HttpHeaders;
+import org.mockserver.client.server.MockServerClient;
+import org.mockserver.junit.MockServerRule;
+import org.mockserver.model.Header;
+
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+/**
+ * @author gongjun[jun.gong@thebeastshop.com]
+ * @since 2017-05-17 16:08
+ */
+public class ErrorMockServer extends MockServerRule {
+
+    public final static String EXPECTED = "{\"status\": \"error\"}";
+
+    public final static Integer port = 5020;
+
+    public ErrorMockServer(Object target) {
+        super(target, port);
+    }
+
+    public void initServer() {
+        MockServerClient mockClient = new MockServerClient("localhost", port);
+        mockClient.when(
+                request()
+                        .withPath("/hello/user")
+                        .withMethod("GET")
+                        .withHeader(new Header(HttpHeaders.ACCEPT, "text/plan"))
+                        .withQueryStringParameter("username",  "foo")
+        )
+        .respond(
+                response()
+                        .withStatusCode(400)
+                        .withBody(EXPECTED)
+        );
+
+    }
+
+}
