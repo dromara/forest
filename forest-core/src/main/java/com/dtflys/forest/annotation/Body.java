70c43520897feb509aee9310407567ae57611823
mySIngleLive <dt_flys@hotmail.com>
2020-08-13 00:27:12 +0800

    refactor: DataBody改名为Body

--- forest-core/src/main/java/com/dtflys/forest/annotation/Body.java
+++ forest-core/src/main/java/com/dtflys/forest/annotation/Body.java
@@ -4,15 +4,23 @@ import java.lang.annotation.*;
 
 /**
  * @author gongjun
- * @since 2020-08-03
+ * @since 2020-08-10
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
-public @interface DataHead {
+public @interface Body {
+
     /**
-     * Head name
+     * URL query name
      * @return
      */
     String value();
+
+    /**
+     * The filters will do some processing for the query value before sending request.
+     * @return
+     */
+    String filter() default "";
+
 }
