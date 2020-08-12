bf245e4b817643598d77a5da6ebf7fef47e1762c
mySIngleLive <dt_flys@hotmail.com>
2020-08-13 00:28:17 +0800

    refactor: DataQuery改名为Query

--- forest-core/src/main/java/com/dtflys/forest/annotation/Query.java
+++ forest-core/src/main/java/com/dtflys/forest/annotation/Query.java
@@ -9,7 +9,7 @@ import java.lang.annotation.*;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
-public @interface DataQuery {
+public @interface Query {
     /**
      * URL query name
      * @return
