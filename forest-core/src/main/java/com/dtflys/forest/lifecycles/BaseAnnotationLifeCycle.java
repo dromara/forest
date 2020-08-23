b92c324e132dbf540bd3f8b6b2d85f2ff5972cc4
mySIngleLive <dt_flys@hotmail.com>
2020-08-23 23:52:11 +0800

    add: BaseAnnotationLifeCycle

---
+++ forest-core/src/main/java/com/dtflys/forest/lifecycles/BaseAnnotationLifeCycle.java
@@ -0,0 +1,27 @@
+package com.dtflys.forest.lifecycles;
+
+import com.dtflys.forest.exceptions.ForestRuntimeException;
+import com.dtflys.forest.http.ForestRequest;
+import com.dtflys.forest.http.ForestResponse;
+import com.dtflys.forest.interceptor.Interceptor;
+import com.dtflys.forest.proxy.InterfaceProxyHandler;
+
+import java.lang.annotation.Annotation;
+
+/**
+ * @author gongjun[dt_flys@hotmail.com]
+ * @since 2020-08-23 23:04
+ */
+public interface BaseAnnotationLifeCycle <A extends Annotation, I> extends Interceptor<I> {
+
+    default void onProxyHandlerInitialized(InterfaceProxyHandler interfaceProxyHandler, A annotation) {
+    }
+
+    @Override
+    default void onError(ForestRuntimeException ex, ForestRequest request, ForestResponse response) {
+    }
+
+    @Override
+    default void onSuccess(I data, ForestRequest request, ForestResponse response) {
+    }
+}
