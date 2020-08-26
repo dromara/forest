49e8052fa4db5f5c742f9e7c38357f52c3f14220
jun.gong <jun.gong@thebeastshop.com>
2020-08-26 11:45:52 +0800

    style: 去掉import java.lang.annotations.*

--- spring-boot-starter-forest/src/main/java/com/thebeastshop/forest/springboot/annotation/EnableForest.java
+++ spring-boot-starter-forest/src/main/java/com/thebeastshop/forest/springboot/annotation/EnableForest.java
@@ -3,7 +3,11 @@ package com.thebeastshop.forest.springboot.annotation;
 import com.thebeastshop.forest.springboot.ForestAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
--- spring-boot-starter-forest/src/main/java/com/thebeastshop/forest/springboot/annotation/ForestScan.java
+++ spring-boot-starter-forest/src/main/java/com/thebeastshop/forest/springboot/annotation/ForestScan.java
@@ -2,7 +2,11 @@ package com.thebeastshop.forest.springboot.annotation;
 
 import org.springframework.context.annotation.Import;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * @author guihuo   (E-mail:1620657419@qq.com)
