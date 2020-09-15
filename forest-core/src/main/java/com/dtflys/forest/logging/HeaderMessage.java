bc38dea9b06352fbefb3fd9637796aaa19d1919d
jun.gong <jun.gong@thebeastshop.com>
2020-09-15 19:25:22 +0800

    add: 日志处理器

---
+++ forest-core/src/main/java/com/dtflys/forest/logging/HeaderMessage.java
@@ -0,0 +1,28 @@
+package com.dtflys.forest.logging;
+
+/**
+ * @author gongjun[jun.gong@thebeastshop.com]
+ * @since 2020-09-14 17:31
+ */
+public class HeaderMessage {
+
+    private String name;
+
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
--- forest-core/src/main/java/com/dtflys/forest/logging/LogHandler.java
+++ forest-core/src/main/java/com/dtflys/forest/logging/LogHandler.java
@@ -1,5 +1,9 @@
 package com.dtflys.forest.logging;
 
+/**
+ * @author gongjun[jun.gong@thebeastshop.com]
+ * @since 2020-09-14 17:31
+ */
 public interface LogHandler {
 
     void logRequest(RequestLogMessage requestLogMessage);
