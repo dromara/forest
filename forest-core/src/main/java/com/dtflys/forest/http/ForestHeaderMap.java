456ade4fe472129b9735fe2815e6fc8e50eab6b9
dt_flys@hotmail.com <jun.gong@thebeastshop.com>
2020-09-22 18:25:03 +0800

    comment: 添加注释

--- forest-core/src/main/java/com/dtflys/forest/http/ForestHeaderMap.java
+++ forest-core/src/main/java/com/dtflys/forest/http/ForestHeaderMap.java
@@ -2,6 +2,13 @@ package com.dtflys.forest.http;
 
 import java.util.*;
 
+/**
+ * Forest请求头Map
+ * <p>该类负责批量管理在Forest请求中所有的请求头信息</p>
+ *
+ * @author gongjun[jun.gong@thebeastshop.com]
+ * @since 2020-08-11 12:45
+ */
 public class ForestHeaderMap {
 
     private final List<ForestHeader> headers;
@@ -14,11 +21,19 @@ public class ForestHeaderMap {
         this.headers = new LinkedList<>();
     }
 
-
+    /**
+     * 获取本请求头集合的大小
+     * @return
+     */
     public int size() {
         return headers.size();
     }
 
+    /**
+     * 根据请求头名称获取请求体的值
+     * @param name 请求头名称
+     * @return
+     */
     public String getValue(String name) {
         ForestHeader header = getHeader(name);
         if (header != null) {
@@ -27,6 +42,11 @@ public class ForestHeaderMap {
         return null;
     }
 
+    /**
+     * 根据请求头名称获取请求体的值列表
+     * @param name 请求头名称
+     * @return
+     */
     public List<String> getValues(String name) {
         List<String> results = new ArrayList<>(2);
         for (ForestHeader header : headers) {
@@ -37,7 +57,11 @@ public class ForestHeaderMap {
         return Collections.unmodifiableList(results);
     }
 
-
+    /**
+     * 根据请求头名称获取请求头对象
+     * @param name 请求头名称
+     * @return
+     */
     public ForestHeader getHeader(String name) {
         for (ForestHeader header : headers) {
             if (header.getName().equalsIgnoreCase(name)) {
@@ -47,6 +71,11 @@ public class ForestHeaderMap {
         return null;
     }
 
+    /**
+     * 根据请求头名称获取请求头对象列表
+     * @param name 请求头名称
+     * @return
+     */
     public List<ForestHeader> getHeaders(String name) {
         List<ForestHeader> results = new ArrayList<>(2);
         for (ForestHeader header : headers) {
@@ -57,7 +86,10 @@ public class ForestHeaderMap {
         return results;
     }
 
-
+    /**
+     * 获取所有请求头的名称列表
+     * @return
+     */
     public List<String> names() {
         List<String> results = new ArrayList<>(headers.size());
         for (ForestHeader header : headers) {
@@ -66,7 +98,10 @@ public class ForestHeaderMap {
         return Collections.unmodifiableList(results);
     }
 
-
+    /**
+     * 获取所有请求头的值
+     * @return
+     */
     public List<String> getValues() {
         List<String> results = new ArrayList<>(headers.size());
         for (ForestHeader header : headers) {
@@ -75,14 +110,29 @@ public class ForestHeaderMap {
         return Collections.unmodifiableList(results);
     }
 
+    /**
+     * 添加请求头
+     * @param header 请求头对象
+     */
     public void addHeader(ForestHeader header) {
         headers.add(header);
     }
 
+    /**
+     * 添加请求头
+     * @param name 请求头名称
+     * @param value 请求头的值
+     */
     public void addHeader(String name, String value) {
         addHeader(new ForestHeader(name, value));
     }
 
+    /**
+     * 设置请求头
+     * 当前设置的请求头名称在本集合中已存在的情况下会覆盖原有同名请求头的值，负责便新增一个请求头
+     * @param name 请求头名称
+     * @param value 请求头的值
+     */
     public void setHeader(String name, String value) {
         ForestHeader header = getHeader(name);
         if (header != null) {
@@ -92,11 +142,18 @@ public class ForestHeaderMap {
         }
     }
 
-
+    /**
+     * 获取本请求头集合的迭代器对象
+     * @return
+     */
     public Iterator<ForestHeader> headerIterator() {
         return headers.iterator();
     }
 
+    /**
+     * 根据请求头的名称删除请求头
+     * @param name
+     */
     public void remove(String name) {
         ForestHeader header = getHeader(name);
         if (header != null) {
