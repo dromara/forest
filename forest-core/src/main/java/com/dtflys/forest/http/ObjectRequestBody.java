1cf2f4fedb7694a93a7745610e0ec43445a24abf
dt_flys@hotmail.com <jun.gong@thebeastshop.com>
2020-09-22 19:20:50 +0800

    comment: 对象请求体相关注释

--- forest-core/src/main/java/com/dtflys/forest/http/ObjectRequestBody.java
+++ forest-core/src/main/java/com/dtflys/forest/http/ObjectRequestBody.java
@@ -1,5 +1,12 @@
 package com.dtflys.forest.http;
 
+/**
+ * 对象类型请求体
+ * <p>该请求体对象会包装一个Java对象，其对象最终会在请求发送前被序列化<p/>
+ *
+ * @author gongjun[jun.gong@thebeastshop.com]
+ * @since 2020-09-22 17:30
+ */
 public class ObjectRequestBody extends ForestRequestBody {
 
     private Object object;
