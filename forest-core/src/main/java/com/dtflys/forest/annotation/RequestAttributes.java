aec02360a1070f9326334bf317ae860d58477619
dt_flys@hotmail.com <jun.gong@thebeastshop.com>
2020-09-23 12:58:57 +0800

    comment: @RequestAttributes注解的相关注释

--- forest-core/src/main/java/com/dtflys/forest/annotation/RequestAttributes.java
+++ forest-core/src/main/java/com/dtflys/forest/annotation/RequestAttributes.java
@@ -6,6 +6,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 该注解可以修饰其他自定义注解，被修饰过的自定义注解中的所有属性都会被自定转换为请求对象的属性
+ *
+ * @author gongjun[dt_flys@hotmail.com]
+ * @since 2020-08-04 19:16
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.ANNOTATION_TYPE)
