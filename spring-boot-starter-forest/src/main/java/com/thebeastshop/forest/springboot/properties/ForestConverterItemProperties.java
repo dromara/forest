c72796b696a7e636d7dc7172e288d4a5bc323f02
dt_flys <dt_flys@hotmail.com>
2020-10-22 19:46:34 +0800

    add: ForestConverterItemProperties类

---
+++ spring-boot-starter-forest/src/main/java/com/thebeastshop/forest/springboot/properties/ForestConverterItemProperties.java
@@ -0,0 +1,29 @@
+package com.thebeastshop.forest.springboot.properties;
+
+import com.dtflys.forest.converter.json.ForestJsonConverter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ForestConverterItemProperties {
+
+    private Class<? extends ForestJsonConverter> type;
+
+    private Map<String, Object> parameters = new HashMap<>();
+
+    public Class<? extends ForestJsonConverter> getType() {
+        return type;
+    }
+
+    public void setType(Class<? extends ForestJsonConverter> type) {
+        this.type = type;
+    }
+
+    public Map<String, Object> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(Map<String, Object> parameters) {
+        this.parameters = parameters;
+    }
+}
