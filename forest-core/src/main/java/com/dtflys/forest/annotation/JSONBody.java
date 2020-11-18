121d8f518607d39a39393768752acc978f882366
mySIngleLive <dt_flys@hotmail.com>
2020-11-19 00:42:12 +0800

    add: @JSONBody注解

--- forest-core/src/main/java/com/dtflys/forest/annotation/JSONBody.java
+++ forest-core/src/main/java/com/dtflys/forest/annotation/JSONBody.java
@@ -18,7 +18,7 @@ import java.lang.annotation.Target;
  *    （1） 键值对模式:<br><br>
  *
  *          <pre class="code">
- *          void send(&#064;Body("username") String username, &#064;Body("password") String password);
+ *          void send(&#064;JSONBody("username") String username, &#064;JSONBody("password") String password);
  *          </pre>
  *
  *          此模式的&#064;JSONBody注解可以修饰一个或多个参数，每个参数仅作为一个键值对。这些键值对最终会被合并成一个JSON对象。<br><br>
@@ -26,7 +26,7 @@ import java.lang.annotation.Target;
  *    （2） 对象模式：<br><br>
  *
  *          <pre class="code">
- *          void send(&#064;Body UserInfo userInfo);
+ *          void send(&#064;JSONBody UserInfo userInfo);
  *          </pre>
  *
  *          此模式的&#064;JSONBody注解可以修饰一个或多个自定义对象，但不能设置名称（value属性）以表明它不是一个键值对。被修饰的参数对象会被拆成一个个字段（这个对象的类中所有getter方法对应的字段），
@@ -35,16 +35,16 @@ import java.lang.annotation.Target;
  *    （3） Map模式：<br><br>
  *
  *          <pre class="code">
- *          void send(&#064;Body Map paramMap);
+ *          void send(&#064;JSONBody Map paramMap);
  *          </pre>
  *
- *          此模式的&#064;Body注解可以修饰一个或多个Map对象，但不能设置名称（value属性）以表明它不是一个键值对。被修饰的参数对象会被拆成一个个键值对（Map中的所有有值的键值对），
+ *          此模式的&#064;JSONBody注解可以修饰一个或多个Map对象，但不能设置名称（value属性）以表明它不是一个键值对。被修饰的参数对象会被拆成一个个键值对（Map中的所有有值的键值对），
  *          所有这些键值对被合并成一个JSON对象。<br><br>
  *
  *    （4） 字符串模式：<br><br>
  *
  *          <pre class="code">
- *          void send(&#064;Body String body);
+ *          void send(&#064;JSONBody String body);
  *          </pre>
  *
  *          此模式的&#064;JSONBody注解可以修饰一个或多个String类型参数，但不能设置名称（value属性）以表明它不是一个键值对。
--- forest-core/src/main/java/com/dtflys/forest/lifecycles/parameter/JSONBodyLifeCycle.java
+++ forest-core/src/main/java/com/dtflys/forest/lifecycles/parameter/JSONBodyLifeCycle.java
@@ -3,12 +3,15 @@ package com.dtflys.forest.lifecycles.parameter;
 import com.dtflys.forest.annotation.JSONBody;
 import com.dtflys.forest.backend.ContentType;
 import com.dtflys.forest.exceptions.ForestRuntimeException;
+import com.dtflys.forest.http.ForestRequest;
 import com.dtflys.forest.lifecycles.ParameterAnnotationLifeCycle;
 import com.dtflys.forest.mapping.MappingParameter;
 import com.dtflys.forest.mapping.MappingVariable;
 import com.dtflys.forest.reflection.ForestMethod;
 import com.dtflys.forest.reflection.MetaRequest;
 import com.dtflys.forest.utils.StringUtils;
+
+import java.lang.reflect.Parameter;
 
 /**
  * Forest &#064;JSONBody注解的生命周期
@@ -31,19 +34,44 @@ public class JSONBodyLifeCycle implements ParameterAnnotationLifeCycle<JSONBody,
             parameter.setObjectProperties(true);
         }
         MetaRequest metaRequest = method.getMetaRequest();
+
+        String methodName = methodName(method);
+
         if (metaRequest == null) {
-            throw new ForestRuntimeException("[Forest] method '" + method.getMethodName() +
-                    "' has not bind a Forest request annotation. Hence annotation @JSONBody cannot be bind on a parameter in this method.");
+            throw new ForestRuntimeException("[Forest] method '" + methodName +
+                    "' has not bind a Forest request annotation. Hence the annotation @JSONBody cannot be bind on a parameter in this method.");
         }
         String contentType = metaRequest.getContentType();
-        if (StringUtils.isNotEmpty(contentType) && !ContentType.APPLICATION_JSON.equals(contentType)) {
+        if (StringUtils.isNotEmpty(contentType) &&
+                !ContentType.APPLICATION_JSON.equals(contentType) &&
+                contentType.indexOf("$") < 0) {
             throw new ForestRuntimeException("[Forest] the Content-Type of request binding on method '" +
-                    method.getMethodName() + "' has already been set value '" + contentType +
-                    "', not 'application/json'. Hence annotation @JSONBody cannot be bind on a parameter in this method.");
+                    methodName + "' has already been set value '" + contentType +
+                    "', not 'application/json'. Hence the annotation @JSONBody cannot be bind on a parameter in this method.");
         }
         metaRequest.setContentType(ContentType.APPLICATION_JSON);
         method.processParameterFilter(parameter, filterName);
         parameter.setTarget(MappingParameter.TARGET_BODY);
         method.addNamedParameter(parameter);
+    }
+
+    private static String methodName(ForestMethod method) {
+        return method.getMethod().toGenericString();
+    }
+
+    @Override
+    public boolean beforeExecute(ForestRequest request) {
+        String contentType = request.getContentType();
+        if (StringUtils.isBlank(contentType)) {
+            request.setContentType(ContentType.APPLICATION_JSON);
+        }
+
+        if (contentType.indexOf(ContentType.APPLICATION_JSON) < 0) {
+            String methodName = methodName(request.getMethod());
+            throw new ForestRuntimeException("[Forest] the Content-Type of request binding on method '" +
+                    methodName + "' has already been set value '" + contentType +
+                    "', not 'application/json'. Hence the annotation @JSONBody cannot be bind on a parameter in this method.");
+        }
+        return true;
     }
 }
