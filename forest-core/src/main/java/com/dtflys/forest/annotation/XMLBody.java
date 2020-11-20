4229f937cebe81f7541f39a42d1ed56295a4176c
dt_flys <dt_flys@hotmail.com>
2020-11-20 14:27:17 +0800

    comment: 修改注释

--- forest-core/src/main/java/com/dtflys/forest/annotation/XMLBody.java
+++ forest-core/src/main/java/com/dtflys/forest/annotation/XMLBody.java
@@ -15,15 +15,7 @@ import java.lang.annotation.Target;
  * <p>同时，请求的 Content-Type 自动被设置为 application/xml</p>
  * <p>该注解有一下几种模式：</p>
  *
- *    （1） 键值对模式:<br><br>
- *
- *          <pre class="code">
- *          void send(&#064;XMLBody("username") String username, &#064;XMLBody("password") String password);
- *          </pre>
- *
- *          此模式的&#064;XMLBody注解可以修饰一个或多个参数，每个参数仅作为一个键值对。这些键值对最终会被合并成一个XML对象。<br><br>
- *
- *    （2） 对象模式：<br><br>
+ *    （1） 对象模式：<br><br>
  *
  *          <pre class="code">
  *          void send(&#064;XMLBody UserInfo userInfo);
@@ -32,16 +24,7 @@ import java.lang.annotation.Target;
  *          此模式的&#064;XMLBody注解可以修饰一个或多个自定义对象，但不能设置名称（value属性）以表明它不是一个键值对。被修饰的参数对象会被拆成一个个字段（这个对象的类中所有getter方法对应的字段），
  *          所有这些字段将像键值对一样被合并成一个XML对象。<br><br>
  *
- *    （3） Map模式：<br><br>
- *
- *          <pre class="code">
- *          void send(&#064;XMLBody Map paramMap);
- *          </pre>
- *
- *          此模式的&#064;XMLBody注解可以修饰一个或多个Map对象，但不能设置名称（value属性）以表明它不是一个键值对。被修饰的参数对象会被拆成一个个键值对（Map中的所有有值的键值对），
- *          所有这些键值对被合并成一个XML对象。<br><br>
- *
- *    （4） 字符串模式：<br><br>
+ *    （2） 字符串模式：<br><br>
  *
  *          <pre class="code">
  *          void send(&#064;XMLBody String body);
@@ -59,13 +42,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface XMLBody {
-
-    /**
-     * 键值对名称（可选参数）
-     * <p>有键值对名称时（有value值时），被该注解修饰的参数将被作为一个键值对合并到请求体中。</p>
-     * <p>没有键值对名称时（没有设value值时），被该注解修饰的参数将被作为一个对象，根据对象的属性拆成一个个键值对，然后合并到请求体中</p>
-     */
-    String value() default "";
 
     /**
      * The filters will do some processing for the query value before sending request.
