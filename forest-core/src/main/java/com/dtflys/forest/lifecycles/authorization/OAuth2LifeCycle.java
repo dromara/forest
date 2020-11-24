865d44b0a1fe9b3dc5056e4892501fb9dbdb4eeb
侯坤林 <houkunlin@aliyun.com>
2020-11-24 09:57:52 +0800

    fix(OAuth2): 修复获取 Token 时判断返回的错误代码键预设不全面（增加微信公众号的errcode）导致本该请求 Token 失败但是并未提示失败问题

--- forest-core/src/main/java/com/dtflys/forest/lifecycles/authorization/OAuth2LifeCycle.java
+++ forest-core/src/main/java/com/dtflys/forest/lifecycles/authorization/OAuth2LifeCycle.java
@@ -249,6 +249,10 @@ public class OAuth2LifeCycle implements MethodAnnotationLifeCycle<OAuth2, Object
      * 缓存 Token 信息的对象.
      */
     public static class TokenCache {
+        /**
+         * 错误代码的KEY，通常在标准 OAuth2 情况下是 error，但是发现在微信公众号的返回是 errcode
+         */
+        private final static String[] ERROR_KEYS = new String[]{"error", "errcode"};
         private final String clientId;
         private final String accessToken;
         private final String refreshToken;
@@ -266,9 +270,13 @@ public class OAuth2LifeCycle implements MethodAnnotationLifeCycle<OAuth2, Object
          * @param token    请求服务器端返回的 Token 结果
          */
         public TokenCache(String clientId, Map<String, Object> token) {
-            Object error = token.get("error");
-            if (error != null) {
-                throw new ForestRuntimeException("OAuth2 获取 Token 失败，服务器返回存在 'error'='" + error + "'");
+            for (String errorKey : ERROR_KEYS) {
+                Object errorValue = token.get(errorKey);
+                if (errorValue != null) {
+                    // 通常可能不会执行到这里，因为一旦 OAuth2 获取失败会返回一个 HTTP CODE 400 ，这个 HTTP CODE 会直接被程序抛出异常
+                    // 但是也有一些 OAuth2 服务器它们会返回 HTTP CODE 200 然后程序走到这里，例如微信公众号开发的服务器就会执行到这里
+                    throw new ForestRuntimeException("OAuth2 获取 Token 失败，服务器返回存在 '" + errorKey + "'='" + errorValue + "'，完整返回错误：" + token);
+                }
             }
             this.clientId = clientId;
             this.accessToken = (String) token.get("access_token");
