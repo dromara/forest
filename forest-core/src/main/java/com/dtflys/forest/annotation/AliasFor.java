80e9b59a127b3aac16a11ad7f384a800501db42a
dt_flys <dt_flys@hotmail.com>
2020-11-25 17:59:00 +0800

    add: @AliasFor注解

---
+++ forest-core/src/main/java/com/dtflys/forest/annotation/AliasFor.java
@@ -0,0 +1,21 @@
+package com.dtflys.forest.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 注解属性别名
+ * <p>为其他注解中定义的属性定义别名，当该属性为空值时将引用其别名对应的属性值</p>
+ *
+ * @author gongjun[dt_flys@hotmail.com]
+ * @since 1.5.0-BETA9
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface AliasFor {
+    String value();
+}
