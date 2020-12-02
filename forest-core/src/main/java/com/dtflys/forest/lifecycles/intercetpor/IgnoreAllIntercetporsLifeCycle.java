059a25827ea52d8f83a2be21e66eb8943ad64bd0
dt_flys <dt_flys@hotmail.com>
2020-12-02 20:01:52 +0800

    add: IgnoreAllIntercetpors注解

---
+++ forest-core/src/main/java/com/dtflys/forest/lifecycles/intercetpor/IgnoreAllIntercetporsLifeCycle.java
@@ -0,0 +1,30 @@
+package com.dtflys.forest.lifecycles.intercetpor;
+
+import com.dtflys.forest.extensions.BasicAuth;
+import com.dtflys.forest.http.ForestRequest;
+import com.dtflys.forest.lifecycles.MethodAnnotationLifeCycle;
+import com.dtflys.forest.reflection.ForestMethod;
+import com.dtflys.forest.utils.Base64Utils;
+
+public class IgnoreAllIntercetporsLifeCycle implements MethodAnnotationLifeCycle<BasicAuth, Object> {
+
+    @Override
+    public void onInvokeMethod(ForestRequest request, ForestMethod method, Object[] args) {
+
+    }
+
+    @Override
+    public boolean beforeExecute(ForestRequest request) {
+        String username = (String) getAttribute(request, "username");
+        String password = (String) getAttribute(request, "password");
+        String basic = "Basic " + Base64Utils.encode(username + ":" + password);
+        request.addHeader("Authorization", basic);
+        return true;
+    }
+
+
+    @Override
+    public void onMethodInitialized(ForestMethod method, BasicAuth annotation) {
+
+    }
+}
