8f78df7850671f8bbf5b318ea6d2dd6958606d4b
dt_flys <dt_flys@hotmail.com>
2021-03-26 18:57:37 +0800

    add: MultipartRequestBody类

---
+++ forest-spring/src/main/java/com/dtflys/forest/http/body/MultipartRequestBody.java
@@ -0,0 +1,34 @@
+package com.dtflys.forest.http.body;
+
+import com.dtflys.forest.exceptions.ForestRuntimeException;
+import com.dtflys.forest.http.ForestRequestBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public class MultipartRequestBody extends ForestRequestBody {
+
+    private MultipartFile multipartFile;
+
+    public MultipartRequestBody(MultipartFile multipartFile) {
+        this.multipartFile = multipartFile;
+    }
+
+
+    public MultipartFile getMultipartFile() {
+        return multipartFile;
+    }
+
+    public void setMultipartFile(MultipartFile multipartFile) {
+        this.multipartFile = multipartFile;
+    }
+
+    @Override
+    public byte[] getByteArray() {
+        try {
+            return multipartFile.getBytes();
+        } catch (IOException e) {
+            throw new ForestRuntimeException(e);
+        }
+    }
+}
