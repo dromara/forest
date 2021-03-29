501338e2f7b27e4540e729176099915b315915be
dt_flys <dt_flys@hotmail.com>
2021-03-29 14:04:28 +0800

    refactor: ForestVariableUndefined.java 改名为 ForestVariableUndefinedException

---
+++ forest-core/src/main/java/com/dtflys/forest/exceptions/ForestVariableUndefinedException.java
@@ -0,0 +1,47 @@
+package com.dtflys.forest.exceptions;
+
+import com.dtflys.forest.utils.StringUtils;
+
+/**
+ * Forest变量未定义异常
+ *
+ * @author gongjun[dt_flys@hotmail.com]
+ * @since v1.5.0
+ */
+public class ForestVariableUndefinedException extends ForestRuntimeException {
+
+    private String variableName;
+
+    private String source;
+
+    public ForestVariableUndefinedException(String variableName) {
+        super(getErrorMessage(variableName, null));
+        this.variableName = variableName;
+    }
+
+
+    public ForestVariableUndefinedException(String variableName, String source) {
+        super(getErrorMessage(variableName, source));
+        this.variableName = variableName;
+    }
+
+    private static String getErrorMessage(String variableName, String source) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[Forest] Cannot resolve variable '");
+        builder.append(variableName);
+        builder.append("'");
+        if (StringUtils.isNotBlank(source)) {
+            builder.append("\n\n\t[Form Template] ");
+            builder.append(source);
+        }
+        return builder.toString();
+    }
+
+    public String getVariableName() {
+        return variableName;
+    }
+
+    public String getSource() {
+        return source;
+    }
+}
