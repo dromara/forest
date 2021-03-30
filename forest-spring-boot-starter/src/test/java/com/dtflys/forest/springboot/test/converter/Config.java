edbd615eb2d2b814f3d3fa67348b193bf9ade34c
caihongming <caihongming@iboxpay.com>
2021-03-30 13:43:22 +0800

    去除文件头

--- forest-spring-boot-starter/src/test/java/com/dtflys/forest/springboot/test/converter/Config.java
+++ forest-spring-boot-starter/src/test/java/com/dtflys/forest/springboot/test/converter/Config.java
@@ -1,16 +1,3 @@
-/*
- * Copyright (C) 2011-2021 ShenZhen iBOXCHAIN Information Technology Co.,Ltd.
- *
- * All right reserved.
- *
- * This software is the confidential and proprietary
- * information of iBOXCHAIN Company of China.
- * ("Confidential Information"). You shall not disclose
- * such Confidential Information and shall use it only
- * in accordance with the terms of the contract agreement
- * you entered into with iBOXCHAIN inc.
- *
- */
 package com.dtflys.forest.springboot.test.converter;
 
 import com.dtflys.forest.converter.ForestConverter;
