3958f3b63d366525e66d36710e0b5cc5b7acde23
LIZONG.WEI <lizong.wei@deltaww.com>
2021-05-05 09:48:35 +0800

    支持全包掃描,可以不指定@ForestScan

--- forest-spring-boot-starter/src/main/java/com/dtflys/forest/springboot/ForestAutoConfiguration.java
+++ forest-spring-boot-starter/src/main/java/com/dtflys/forest/springboot/ForestAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.dtflys.forest.springboot;
 
+import com.dtflys.forest.springboot.annotation.ForestScannerRegister;
 import com.dtflys.forest.springboot.properties.ForestConfigurationProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -7,10 +8,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan("com.dtflys.forest.springboot.properties")
 @EnableConfigurationProperties({ForestConfigurationProperties.class})
+@Import({ForestScannerRegister.class})
 public class ForestAutoConfiguration {
 
     @Autowired
--- forest-spring-boot-starter/src/main/java/com/dtflys/forest/springboot/annotation/ForestScannerRegister.java
+++ forest-spring-boot-starter/src/main/java/com/dtflys/forest/springboot/annotation/ForestScannerRegister.java
@@ -1,6 +1,10 @@
 package com.dtflys.forest.springboot.annotation;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.annotation.AnnotationAttributes;
@@ -11,12 +15,13 @@ import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author guihuo   (E-mail:1620657419@qq.com)
  * @since 2018-09-25 11:59
  */
-public class ForestScannerRegister implements ImportBeanDefinitionRegistrar, ResourceLoaderAware {
+public class ForestScannerRegister implements BeanFactoryAware, ImportBeanDefinitionRegistrar, ResourceLoaderAware {
 
 
     private ResourceLoader resourceLoader;
@@ -25,6 +30,8 @@ public class ForestScannerRegister implements ImportBeanDefinitionRegistrar, Res
 
     public static String configurationId;
 
+    private BeanFactory beanFactory;
+
     @Override
     public void setResourceLoader(ResourceLoader resourceLoader) {
         this.resourceLoader = resourceLoader;
@@ -32,26 +39,30 @@ public class ForestScannerRegister implements ImportBeanDefinitionRegistrar, Res
 
     @Override
     public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
-        AnnotationAttributes annoAttrs = AnnotationAttributes.fromMap(importingClassMetadata.getAnnotationAttributes(ForestScan.class.getName()));
 
-
-        for (String pkg : annoAttrs.getStringArray("value")) {
-            if (StringUtils.hasText(pkg)) {
-                basePackages.add(pkg);
+        Map<String, Object> annotationAttributes = importingClassMetadata.getAnnotationAttributes(ForestScan.class.getName());
+        if (annotationAttributes == null) {
+            ForestScannerRegister.basePackages.addAll(AutoConfigurationPackages.get(beanFactory));
+        } else {
+            AnnotationAttributes annoAttrs = AnnotationAttributes.fromMap(annotationAttributes);
+            for (String pkg : annoAttrs.getStringArray("value")) {
+                if (StringUtils.hasText(pkg)) {
+                    basePackages.add(pkg);
+                }
             }
-        }
 
-        for (String pkg : annoAttrs.getStringArray("basePackages")) {
-            if (StringUtils.hasText(pkg)) {
-                basePackages.add(pkg);
+            for (String pkg : annoAttrs.getStringArray("basePackages")) {
+                if (StringUtils.hasText(pkg)) {
+                    basePackages.add(pkg);
+                }
             }
-        }
 
-        for (Class<?> clazz : annoAttrs.getClassArray("basePackageClasses")) {
-            basePackages.add(ClassUtils.getPackageName(clazz));
-        }
+            for (Class<?> clazz : annoAttrs.getClassArray("basePackageClasses")) {
+                basePackages.add(ClassUtils.getPackageName(clazz));
+            }
 
-        configurationId = annoAttrs.getString("configuration");
+            configurationId = annoAttrs.getString("configuration");
+        }
 
 //        ClassPathClientScanner scanner = new ClassPathClientScanner(configurationId, registry);
 //        // this check is needed in Spring 3.1
@@ -64,4 +75,8 @@ public class ForestScannerRegister implements ImportBeanDefinitionRegistrar, Res
 
     }
 
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
 }
