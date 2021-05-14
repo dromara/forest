e980b2f96ab7d721d8143701079411c00ecacd69
dt_flys <dt_flys@hotmail.com>
2021-05-14 19:28:01 +0800

    test:#I3P1R0 新增强制UrlEncoded的字符串模板

--- forest-core/src/test/java/com/dtflys/test/http/client/GetClient.java
+++ forest-core/src/test/java/com/dtflys/test/http/client/GetClient.java
@@ -167,7 +167,7 @@ public interface GetClient {
 
 
     @Request(
-            url = "http://localhost:${port}/hello/user?username=${0}",
+            url = "http://localhost:${port}/hello/user?username={0}",
             headers = {
                     "Accept:text/plain",
                     "Content-Type: application/json"
@@ -251,7 +251,7 @@ public interface GetClient {
     @Request(
             url = "http://localhost:${port}/hello/user",
             headers = {"Accept:text/plain"},
-            data = "username=${username}"
+            data = "username={username}"
     )
     String varParamGet(@DataVariable("username") String username);
 
@@ -290,7 +290,7 @@ public interface GetClient {
             async = true,
             headers = {"Accept:text/plain"},
             timeout = 3000,
-            data = "username=${ username.toString() }"
+            data = "username={ username.toString() }"
     )
     Future<String> asyncVarParamGet(@DataVariable("username") String username, OnSuccess<String> onSuccess, OnError onError);
 
@@ -299,7 +299,7 @@ public interface GetClient {
 
 
     @Get(
-            url = "http://localhost:${port}/hello/user?${name}",
+            url = "http://localhost:${port}/hello/user?{name}",
             headers = {"Accept:text/plain"}
     )
     String getWithQueryString(@DataVariable("name") String name);
