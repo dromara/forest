e59b3e7da04eae5f2371c007723191b339b1c0ed
dt_flys <dt_flys@hotmail.com>
2021-05-14 19:53:32 +0800

    add: ForestTemplateSyntaxError异常类

---
+++ forest-core/src/main/java/com/dtflys/forest/exceptions/ForestTemplateSyntaxError.java
@@ -0,0 +1,8 @@
+package com.dtflys.forest.exceptions;
+
+public class ForestTemplateSyntaxError extends ForestRuntimeException {
+
+    public ForestTemplateSyntaxError(String message) {
+        super(message);
+    }
+}
