a1de778d5e943c095f5b2a21886e3fe5b4901aee
dt_flys <dt_flys@hotmail.com>
2021-09-15 20:04:45 +0800

    comment: Forest 通用客户端接口注释

--- forest-core/src/main/java/com/dtflys/forest/ForestGenericClient.java
+++ forest-core/src/main/java/com/dtflys/forest/ForestGenericClient.java
@@ -4,11 +4,26 @@ import com.dtflys.forest.annotation.Request;
 import com.dtflys.forest.http.ForestRequest;
 import com.dtflys.forest.http.ForestResponse;
 
+/**
+ * Forest 通用客户端接口
+ */
 public interface ForestGenericClient {
 
+    /**
+     * 获取 Forest 通用请求
+     *
+     * @return {@link ForestRequest} 实例
+     */
     @Request("http://localhost/")
     ForestRequest<?> request();
 
+    /**
+     * 获取 Forest 通用请求
+     *
+     * @param clazz 带泛型参数的 {@link Class} 对象
+     * @param <T> 泛型参数
+     * @return {@link ForestRequest} 实例
+     */
     @Request("http://localhost/")
     <T> ForestRequest<T> request(Class<T> clazz);
 }
