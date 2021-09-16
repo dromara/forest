c9854edff4bfa6c004591994b4faa77d3f7a7c43
dt_flys <dt_flys@hotmail.com>
2021-09-16 20:12:53 +0800

    add: SuccessLifeCycle生命周期类

---
+++ forest-core/src/main/java/com/dtflys/forest/lifecycles/method/SuccessLifeCycle.java
@@ -0,0 +1,26 @@
+package com.dtflys.forest.lifecycles.method;
+
+import com.dtflys.forest.annotation.Success;
+import com.dtflys.forest.http.ForestRequest;
+import com.dtflys.forest.lifecycles.MethodAnnotationLifeCycle;
+import com.dtflys.forest.reflection.ForestMethod;
+
+/**
+ * 重试注解的生命周期类
+ *
+ * @author gongjun [dt_flys@hotmail.com]
+ */
+public class SuccessLifeCycle implements MethodAnnotationLifeCycle<Success, Object> {
+
+    @Override
+    public void onMethodInitialized(ForestMethod method, Success annotation) {
+        method.setExtensionParameterValue("successAnnotation", annotation);
+    }
+
+    @Override
+    public void onInvokeMethod(ForestRequest request, ForestMethod method, Object[] args) {
+        Success annotation = (Success) request.getMethod().getExtensionParameterValue("successAnnotation");
+    }
+
+
+}
