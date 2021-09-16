a6258be3c716f1612007db7aabc10a9b17151837
dt_flys <dt_flys@hotmail.com>
2021-09-16 20:12:35 +0800

    add: @Success注解类

---
+++ forest-core/src/main/java/com/dtflys/forest/annotation/Success.java
@@ -0,0 +1,19 @@
+package com.dtflys.forest.annotation;
+
+import com.dtflys.forest.callback.SuccessWhen;
+import com.dtflys.forest.lifecycles.method.RetryLifeCycle;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@MethodLifeCycle(RetryLifeCycle.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface Success {
+
+    Class<? extends SuccessWhen> condition();
+}
