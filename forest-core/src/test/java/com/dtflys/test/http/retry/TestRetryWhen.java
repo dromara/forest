080a441662108e85ad121d10f91c93a7ee025b08
dt_flys <dt_flys@hotmail.com>
2021-09-23 19:14:16 +0800

    comment: 添加测试相关注释

--- forest-core/src/test/java/com/dtflys/test/http/retry/TestRetryWhen.java
+++ forest-core/src/test/java/com/dtflys/test/http/retry/TestRetryWhen.java
@@ -6,6 +6,12 @@ import com.dtflys.forest.http.ForestResponse;
 
 public class TestRetryWhen implements RetryWhen {
 
+    /**
+     * 请求重试条件
+     * @param request Forest请求对象
+     * @param response Forest响应对象
+     * @return 是否重试
+     */
     @Override
     public boolean retryWhen(ForestRequest request, ForestResponse response) {
         return response.statusIs(203);
