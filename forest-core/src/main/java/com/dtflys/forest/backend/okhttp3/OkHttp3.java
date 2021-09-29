11cf5cf55bccdb8de77994d0636e0a6273fb146d
mySIngleLive <dt_flys@hotmail.com>
2021-09-30 00:39:39 +0800

    add: @OkHttp3 注解

---
+++ forest-core/src/main/java/com/dtflys/forest/backend/okhttp3/OkHttp3.java
@@ -0,0 +1,26 @@
+package com.dtflys.forest.backend.okhttp3;
+
+
+import com.dtflys.forest.annotation.Backend;
+import com.dtflys.forest.backend.HttpBackendSelector;
+import com.dtflys.forest.backend.okhttp3.OkHttp3Backend;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 后端注解: OkHttp3
+ * <p>使用该注解可以指定请求后端框架为 OkHttp3
+ *
+ * @author gongjun[dt_flys@hotmail.com]
+ * @since 1.5.5
+ */
+@Backend(OkHttp3Backend.NAME)
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+public @interface OkHttp3 {
+}
