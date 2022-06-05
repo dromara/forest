4982cfbd03669948426c8cbe244dd0dde50b7ed5
dt_flys <dt_flys@hotmail.com>
2022-06-05 12:09:14 +0800

    update: spring生命周期

--- forest-spring-boot-starter/src/main/java/com/dtflys/forest/springboot/ForestAutoConfiguration.java
+++ forest-spring-boot-starter/src/main/java/com/dtflys/forest/springboot/ForestAutoConfiguration.java
@@ -6,26 +6,19 @@ import com.dtflys.forest.reflection.SpringForestObjectFactory;
 import com.dtflys.forest.spring.ForestBeanProcessor;
 import com.dtflys.forest.springboot.annotation.ForestScannerRegister;
 import com.dtflys.forest.springboot.properties.ForestConfigurationProperties;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Role;
 
 import javax.annotation.Resource;
 
 @Configuration(proxyBeanMethods = false)
-@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-@ComponentScan("com.dtflys.forest.springboot.properties")
 @EnableConfigurationProperties({ForestConfigurationProperties.class})
 @Import({ForestScannerRegister.class})
 public class ForestAutoConfiguration {
@@ -39,7 +32,6 @@ public class ForestAutoConfiguration {
         return new SpringForestProperties();
     }
 
-
     @Bean
     @ConditionalOnMissingBean
     public SpringForestObjectFactory forestObjectFactory() {
@@ -52,21 +44,25 @@ public class ForestAutoConfiguration {
         return new SpringInterceptorFactory();
     }
 
-
     @Bean
+    @DependsOn("forestBeanProcessor")
     @ConditionalOnMissingBean
     public ForestBeanRegister forestBeanRegister(SpringForestProperties properties,
                                                     SpringForestObjectFactory forestObjectFactory,
                                                     SpringInterceptorFactory forestInterceptorFactory,
                                                     ForestConfigurationProperties forestConfigurationProperties) {
-        ForestBeanRegister register = new ForestBeanRegister(applicationContext, forestConfigurationProperties);
-        register.registerForestConfiguration(properties, forestObjectFactory, forestInterceptorFactory, forestConfigurationProperties);
-        register.registerScanner(forestConfigurationProperties);
+        ForestBeanRegister register = new ForestBeanRegister(
+                applicationContext,
+                forestConfigurationProperties,
+                properties,
+                forestObjectFactory,
+                forestInterceptorFactory);
+        register.registerForestConfiguration();
+        register.registerScanner();
         return register;
     }
 
     @Bean
-    @Lazy
     @ConditionalOnMissingBean
     public ForestBeanProcessor forestBeanProcessor() {
         return new ForestBeanProcessor();
