34444c4d30ad67f775a1cc9f11d34879c0c5431e
dt_flys <dt_flys@hotmail.com>
2022-08-03 19:16:21 +0800

    refactor: 简化代码

--- forest-core/src/test/java/com/dtflys/test/http/backendclient/MyHttpClientProvider.java
+++ forest-core/src/test/java/com/dtflys/test/http/backendclient/MyHttpClientProvider.java
@@ -1,16 +1,13 @@
 package com.dtflys.test.http.backendclient;
 
 import com.dtflys.forest.backend.httpclient.HttpClientProvider;
-import com.dtflys.forest.backend.okhttp3.OkHttpClientProvider;
 import com.dtflys.forest.handler.LifeCycleHandler;
 import com.dtflys.forest.http.ForestRequest;
-import okhttp3.OkHttpClient;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyHttpClientProvider implements HttpClientProvider {
@@ -20,8 +17,14 @@ public class MyHttpClientProvider implements HttpClientProvider {
     @Override
     public HttpClient getClient(ForestRequest request, LifeCycleHandler lifeCycleHandler) {
         count.incrementAndGet();
+        RequestConfig config = RequestConfig.custom()
+                .setConnectTimeout(700 * 1000)
+                .setSocketTimeout(700 * 1000)
+                .setConnectionRequestTimeout(700 * 1000)
+                .setRedirectsEnabled(false)
+                .build();
         CloseableHttpClient httpClient = HttpClients.custom()
-                .setDefaultRequestConfig(RequestConfig.custom().setStaleConnectionCheckEnabled(true).build())
+                .setDefaultRequestConfig(config)
                 .build();
         return httpClient;
     }
