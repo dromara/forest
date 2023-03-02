884f718a3abd9f441e4c4f563801aea7ab483e33
noear <noear@live.cn>
2023-03-02 20:59:12 +0800

    forest-solon-plugin：升级 solon 为 2.2.0，并调整单测代码

--- forest-spring-boot-starter/src/test/java/com/dtflys/forest/springboot/test/async/TestAsync.java
+++ forest-spring-boot-starter/src/test/java/com/dtflys/forest/springboot/test/async/TestAsync.java
@@ -118,6 +118,7 @@ public class TestAsync {
         }
         try {
             for (int i = 0; i < size; i++) {
+                System.out.println("=====" + i);
                 Forest.head("/")
                         .host("localhost")
                         .port(server.getPort())
