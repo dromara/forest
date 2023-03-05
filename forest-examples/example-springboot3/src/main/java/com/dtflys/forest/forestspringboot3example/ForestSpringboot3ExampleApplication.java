d01d563e49cd6f1726a96be52fd8bfedaeaec797
dt_flys <dt_flys@hotmail.com>
2023-03-06 02:03:28 +0800

    add: forest 示例工程

--- forest-examples/example-springboot3/src/main/java/com/dtflys/forest/forestspringboot3example/ForestSpringboot3ExampleApplication.java
+++ forest-examples/example-springboot3/src/main/java/com/dtflys/forest/forestspringboot3example/ForestSpringboot3ExampleApplication.java
@@ -10,7 +10,6 @@ public class ForestSpringboot3ExampleApplication {
 
 
     public static void main(String[] args) {
-        System.setProperty("jdk.tls.client.protocols", "TLSv1.2");
         SpringApplication.run(ForestSpringboot3ExampleApplication.class, args);
     }
 
