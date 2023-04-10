34d448dd0e850716af8b190e1bc9063118dd0684
公子骏 <dt_flys@hotmail.com>
2023-04-10 14:35:54 +0800

    update: ChatGPT的接入示例

---
+++ forest-examples/example-chatgpt/src/main/java/com/dtflys/forest/example/ChatGPTExampleApplication.java
@@ -0,0 +1,29 @@
+package com.dtflys.forest.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
+
+@SpringBootApplication
+public class ChatGPTExampleApplication implements CommandLineRunner {
+
+    @Resource
+    private ChatGPT chatGPT;
+
+    @Override
+    public void run(String... args) throws Exception {
+        GPTResponse response = chatGPT.send("你好");
+        System.out.println(response.getChoices().get(0).getText());
+    }
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(ChatGPTExampleApplication.class, args);
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
+    }
+
+}
