fd202bd267518eaf117a4d4a3306ebf5f278923f
dt_flys <dt_flys@hotmail.com>
2025-05-30 15:26:28 +0800

    refactor: OkHttp3 后端分离

--- forest-core/src/main/java/com/dtflys/forest/logging/LogBodyMessage.java
+++ forest-core/src/main/java/com/dtflys/forest/logging/LogBodyMessage.java
@@ -4,8 +4,6 @@ package com.dtflys.forest.logging;
 /**
  * 请求体日志消息接口
  *
- * @see com.dtflys.forest.backend.okhttp3.logging.OkHttp3LogBodyMessage
- * @see com.dtflys.forest.backend.httpclient.logging.HttpclientLogBodyMessage
  * @author gongjun[jun.gong@thebeastshop.com]
  * @since 2020-09-14 17:10
  */
